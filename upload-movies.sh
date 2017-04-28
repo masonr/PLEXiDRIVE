ce79f795f9d3969e98c96bb61be172b205db56db
Mason Rowe <masonr@udel.edu>
2017-04-28 19:26:54 -0400

    Script to upload movies to gdrive

---
+++ upload-movies.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Directory where this file exists
+plexidrive_dir=`dirname $0`
+cd "$plexidrive_dir"
+
+# Read in configuration file
+if [ -e ./plexidrive.conf ] ; then
+	source ./plexidrive.conf
+else
+	echo "Configuration file - plexidrive.conf - not found."
+	echo "$(date +%F_%T) Configuration file - plexidrive.conf - not found." >> "$plexidrive_dir/upload-error"
+	exit 1
+fi
+
+# Create blank files if they don't exist yet
+touch ./plex-scan
+touch ./upload-error
+touch ./gdrive-directory
+
+# Check variables
+if [ "$num_of_gdrives" -ne ${#drive_names[@]} ] || [ "$num_of_gdrives" -ne ${#gdrive_config_paths[@]} ] || [ "$num_of_gdrives" -ne ${#gdrive_mount_paths[@]} ] ; then
+	echo "gdrive variables are not set up correctly! Exiting..."
+	echo "$(date +%F_%T) gdrive variables are not set up correctly! Exiting." >> "$plexidrive_dir/upload-error"
+	exit 1
+fi
+
+###################################################################################
+################################## MOVIE UPLOADS ##################################
+###################################################################################
+
+# Loop through to see if any files are done downloading
+IFS=$'\n';
+for f in $(find "$local_movies_path" -regextype posix-egrep -regex ".*\.($file_types)$"); do
+
+	# Set up variables and folder
+	path=${f%/*}
+	cd "$path"
+	folder=`echo $f | cut -d'/' -f5`
+	f=${f##*/}
+	echo "File: $f"
+
+	# Upload file to every Google drive account
+	for (( i=0; i<${num_of_gdrives}; i++ ));
+	do
+		wait_time=1
+
+		while true
+		do
+			# Create the movie folder within the Movies directory on drive
+			out=`gdrive --config ${gdrive_config_paths[i]} mkdir --parent ${mov_root##*::} "$folder"`
+			mov_folder=`echo $out | head -n1 | awk '{print $2;}'`
+		
+			if [[ $out == *"Error"* ]] && [[ $out == *"rateLimitExceeded"* ]]
+			then
+				wait_time=$((wait_time * 2))
+				echo "Rate Limit Exceeded, waiting $wait_time seconds."
+				echo "$(date +%F_%T) RateLimitExceeded mov:${drive_names[i]}:$folder:$out" >> "$plexidrive_dir/upload-error"
+				sleep $wait_time
+			elif [[ $out =~ "Error \d{3}" ]] && [[ $out != *"rateLimitExceeded"* ]]
+			then
+				echo "$(date +%F_%T) UnknownError mov:${drive_names[i]}:$folder:$out" >> "$plexidrive_dir/upload-error"
+				exit 0 # exit script
+			else
+				# Upload file to the movie folder
+				echo "Uploading file to ${drive_names[i]}......"
+				while true
+				do
+					result=`gdrive --config ${gdrive_config_paths[i]} upload --parent "$mov_folder" $f`
+					
+					# Check result of upload
+					if [[ $result == *"Error"* ]] && [[ $result == *"rateLimitExceeded"* ]]
+					then
+						wait_time=$((wait_time * 2))
+						echo "Rate Limit Exceeded, waiting $wait_time seconds"
+						echo "$(date +%F_%T) RateLimitExceeded mov:${drive_names[i]}:$folder:$out" >> "$plexidrive_dir/upload-error"
+						sleep $wait_time					
+					elif [[ $out =~ "Error \d{3}" ]] && [[ $out != *"rateLimitExceeded"* ]]
+					then
+						echo "Unknown error! Halting program."
+						gdrive --config ${gdrive_config_paths[i]} delete $mov_folder
+						echo "$(date +%F_%T) UnknownError mov:${drive_names[i]}:$folder:$out" >> "$plexidrive_dir/upload-error"
+						exit 0 # terminate the script
+					else
+						echo "success!"
+						break # exit loop
+					fi
+				done
+				break # exit loop
+			fi
+		done
+
+	echo "mov:$folder" >> "$plexidrive_dir/plex-scan"
+
+	# Delete local file after successful upload, if enabled
+	if [ "$delete_after_upload" = true ] ; then
+		# Delete the local folder
+		if [ "${local_movies_path##*/}" = "$folder" ]
+		then
+			rm $f # delete file if in local movie directory
+		else
+			rm -rf $path # delete movie folder
+		fi	
+	done
+done
